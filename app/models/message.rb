7367661e0e333545d156a43227a112bf89c5767a
Neru-dum <tanten180@gmail.com>
2020-02-19 19:03:53 +0900

    add リサイズ問題

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,8 @@
 class Message < ApplicationRecord
-  mount_uploader :image, ImageUploader
   belongs_to :user
   belongs_to :group
   
   validates :content, presence: true, unless: :image?
+  
+  mount_uploader :image, ImageUploader
 end
